bc0d84942a68b3130e8083415416bedc0a7373a6
akakream <akakream@gmail.com>
2023-09-29 11:25:24 +0200

    more makeupppp

--- pkg/routes/ui_routes.go
+++ pkg/routes/ui_routes.go
@@ -1,12 +1,13 @@
 package routes
 
 import (
-	"github.com/akakream/DistroMash/controllers"
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/akakream/DistroMash/controllers"
 )
 
 func UiRoutes(app *fiber.App) {
-	app.Get("/", controllers.GetHomeUI)
+	app.Get("/", controllers.GetStrategyListUI)
 
 	app.Get("/crdt", controllers.GetCrdtListUI)
 	app.Get("/strategy", controllers.GetStrategyListUI)
